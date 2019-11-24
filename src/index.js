002d4f4d2460880bbc0bbe0b0989dc6c5f97a72a
Mag80lvl <kventi.d@yandex.ru>
2019-11-24 22:42:19 +0300

    socket server init

--- src/index.js
+++ src/index.js
@@ -1,18 +1,47 @@
 import express from "express"
-import bodyParser from "body-parser"
-import mongoose from "mongoose"
-import cors from 'cors'
+import { createServer } from 'http';
+import socket from 'socket.io';
 
-import TestController from "./controller/TestController"
-const Test = new TestController()
+// import bodyParser from "body-parser"
+// import mongoose from "mongoose"
+// import cors from 'cors'
+
+// import TestController from "./controller/TestController"
+// const Test = new TestController()
 
 const app = express()  
-app.use(cors())
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+const http = createServer(app)
+const io = socket(http);
 
-mongoose.connect("mongodb://localhost/chat")
+// app.use(cors())
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(bodyParser.json())
 
-app.get("/test",Test.index)
+// mongoose.connect("mongodb://localhost/chat")
 
-app.listen(3333,() => {console.log("server it`s work!")})
+// app.get("/test",Test.index)
+
+app.get('/', function(req, res){
+  res.send('ok');
+});
+
+io.on('connection', function(socket){
+
+  socket.broadcast.emit('userConnected', {
+    data : `userConnected ${socket.id}`
+  })  
+
+  socket.on('disconnect', function(){
+    socket.broadcast.emit('userDisconnected', {
+      data : `disconected ${socket.id}`
+    })
+  });
+
+  socket.on('chatMessage', function (msg) {
+    console.log(`client say "${msg}"`)
+
+    io.emit('serverMessage',msg);
+  })
+});
+
+http.listen(3333,() => {console.log("server it`s work!")})
